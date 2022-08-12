c9f1c20a39e152c52877b0943548c2a249b8c941
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-08-12 21:23:17 +1000

    create vvdec script

---
+++ buildscripts/scripts/vvdec.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+. ../../include/path.sh
+
+if [ "$1" == "build" ]; then
+	true
+elif [ "$1" == "clean" ]; then
+	rm -rf _build$ndk_suffix
+	exit 0
+else
+	exit 255
+fi
+
+mkdir -p _build$ndk_suffix
+cd _build$ndk_suffix
+
+extra=
+[[ "$ndk_vvdec" == "armeabi-v7a"* ]] && extra="-DANDROID_ARM_NEON=TRUE"
+
+cmake \
+       -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI=$ndk_vvdec \
+	    -DCMAKE_TOOLCHAIN_FILE=${HOME}/mpv-android-vvc/buildscripts/sdk/android-ndk-r25/build/cmake/android.toolchain.cmake \
+		-DANDROID_STL=c++_shared -DANDROID_PLATFORM=android-$MINSDKVERSION \
+	    -DCMAKE_INSTALL_PREFIX=$prefix_dir $extra ..
+
+cmake --build . --config release -j$cores
+cmake --build . --target install
