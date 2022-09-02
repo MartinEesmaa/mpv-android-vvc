0061b376b938c5e827c0e666c8a5525c7f3490c8
Martin Eesmaa <martin.eesmaa@protonmail.com>
2022-09-03 00:00:28 +1000

    revert back

--- buildscripts/scripts/vvdec.sh
+++ buildscripts/scripts/vvdec.sh
@@ -19,7 +19,7 @@ extra=
 
 cmake \
        -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI=$ndk_vvdec \
-	    -DCMAKE_TOOLCHAIN_FILE=${HOME}/mpv-android-vvc/buildscripts/sdk/android-ndk-r25/build/cmake/android-legacy.toolchain.cmake \
+	    -DCMAKE_TOOLCHAIN_FILE=${HOME}/mpv-android-vvc/buildscripts/sdk/android-ndk-r24/build/cmake/android.toolchain.cmake \
 		-DANDROID_STL=c++_shared -DANDROID_PLATFORM=android-$MINSDKVERSION \
 	    -DCMAKE_INSTALL_PREFIX=$prefix_dir $extra ..
 
