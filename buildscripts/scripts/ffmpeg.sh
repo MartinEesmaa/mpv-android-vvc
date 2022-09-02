584c918d42706fa5208adb0879909cf09d10d841
Martin Eesmaa <martin.eesmaa@protonmail.com>
2022-09-02 21:44:34 +1000

    add libopus in ffmpeg.sh to improve decode quality

--- buildscripts/scripts/ffmpeg.sh
+++ buildscripts/scripts/ffmpeg.sh
@@ -26,7 +26,7 @@ cpuflags=
 	--target-os=android --enable-cross-compile --cross-prefix=$ndk_triple- --cc=$CC \
 	--arch=${ndk_triple%%-*} --cpu=$cpu --pkg-config=pkg-config \
 	--extra-cflags="-I$prefix_dir/include $cpuflags" --extra-ldflags="-L$prefix_dir/lib" \
-	--enable-{jni,mediacodec,mbedtls,libdav1d,libvvdec,libfdk-aac,libxml2} --disable-vulkan \
+	--enable-{jni,mediacodec,mbedtls,libdav1d,libvvdec,libfdk-aac,libxml2,libopus} --disable-vulkan \
 	--enable-version3 \
 	--disable-static --enable-shared \
 	--disable-{stripping,doc,programs} \
