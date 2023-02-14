8983856f2dfc43ac654411e5fd9225c0f58aca47
MartinEesmaa <martin.eesmaa@outlook.com>
2023-02-14 22:39:25 +1100

    change back to original android ndk 24

--- buildscripts/scripts/vvdec.sh
+++ buildscripts/scripts/vvdec.sh
@@ -19,8 +19,8 @@ extra=
 
 cmake \
        -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI=$ndk_vvdec \
-	    -DCMAKE_TOOLCHAIN_FILE=${HOME}/mpv-android-vvc/buildscripts/sdk/android-ndk-r25b/build/cmake/android.toolchain.cmake \
-		-DANDROID_STL=c++_shared -DVVDEC_ENABLE_LINK_TIME_OPT=OFF -DANDROID_PLATFORM=android-21 \
+	    -DCMAKE_TOOLCHAIN_FILE=${HOME}/mpv-android-vvc/buildscripts/sdk/android-ndk-r24/build/cmake/android.toolchain.cmake \
+		-DANDROID_STL=c++_shared -DANDROID_PLATFORM=android-$MINSDKVERSION \
 	    -DCMAKE_INSTALL_PREFIX=$prefix_dir $extra ..
 
 cmake --build . --config release -j$cores
