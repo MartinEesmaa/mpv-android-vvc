7f9abe480f7958b52f8c965a89a4f9549aa29aca
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-10-19 23:14:38 +1100

    Force find iconv library on libxml2

--- buildscripts/scripts/libxml2.sh
+++ buildscripts/scripts/libxml2.sh
@@ -22,7 +22,7 @@ extra=
 cmake \
        -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=$ndk_vvdec \
 	    -DCMAKE_TOOLCHAIN_FILE=${HOME}/mpv-android-vvc/buildscripts/sdk/android-ndk-$v_ndk/build/cmake/android.toolchain.cmake \
-		-DBUILD_SHARED_LIBS=ON -DANDROID_PLATFORM=android-21 -DLIBXML2_WITH_PYTHON=OFF \
+		-DBUILD_SHARED_LIBS=ON -DANDROID_PLATFORM=android-21 -DLIBXML2_WITH_PYTHON=OFF -DIconv_LIBRARY=$prefix_dir/$prefix_name/lib \
 	    -DCMAKE_INSTALL_PREFIX=$prefix_dir $extra ..
 
 cmake --build . --config release -j$cores
