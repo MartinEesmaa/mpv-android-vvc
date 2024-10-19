26994e255b3f605240c0b8763f02e570824ac472
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-10-19 23:04:51 +1100

    Fix configure not found on libxml2
    This will use autogen.sh execute before configure. - Martin Eesmaa

--- buildscripts/scripts/libxml2.sh
+++ buildscripts/scripts/libxml2.sh
@@ -13,12 +13,18 @@ else
 	exit 255
 fi
 
+[ -f configure ] || ./autogen.sh
+
 mkdir -p $build
 cd $build
 
+extra=
+[[ "$ndk_triple" == "i686"* ]] && extra="--disable-asm"
+
 ../configure \
-	--host=$ndk_triple --with-pic \
-	--enable-static --disable-shared
+	--host=$ndk_triple $extra \
+	--enable-shared --disable-static \
+	--disable-require-system-font-provider
 
 make -j$cores
 make DESTDIR="$prefix_dir" install
